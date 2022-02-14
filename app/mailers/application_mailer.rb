26a563038f9184ea0db4c5e0fc6c13b230d0a455
shinpei-hattori <hatto08101127@gmail.com>
2022-02-15 03:51:09 +0900

    微修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  if Rails.env = "development"
-    default from: 'fodelionline@gmail.com'
-  else
+    # default from: 'fodelionline@gmail.com'
     default from: 'noreply@fodelionline.site'
-  end
+
   layout 'mailer'
 end
