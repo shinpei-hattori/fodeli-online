1259580ddeb5fbc6ba0417f1b9d1b116c074220f
shinpei-hattori <hatto08101127@gmail.com>
2022-02-19 05:58:57 +0900

    aboutページの作成

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -53,23 +53,33 @@ RSpec.describe "Sessions", type: :system do
       end
 
       it "有効なユーザーでログインする前後でヘッダーが正しく表示されていることを確認" do
+        expect(page).to have_link 'ホーム', href: root_path
         expect(page).to have_link '使い方', href: about_path
         expect(page).to have_link '新規登録', href: signup_path
         expect(page).to have_link 'ログイン', href: login_path
-        expect(page).not_to have_link 'ログアウト', href: logout_path
         expect(page).not_to have_link 'ユーザー一覧', href: users_path
+        expect(page).not_to have_link 'チャットを探す', href: chat_rooms_path
+        expect(page).not_to have_link '参加チャット', href: chatlists_user_path(user)
         expect(page).not_to have_link '個人チャット履歴', href: dmlists_user_path(user)
+        expect(page).not_to have_link 'プロフィール', href: user_path(user)
+        expect(page).not_to have_link '編集', href: edit_user_path(user)
+        expect(page).not_to have_link 'ログアウト', href: logout_path
 
         fill_in "session_email", with: user.email
         fill_in "session_password", with: user.password
         click_button "ログイン"
 
+        expect(page).to have_link 'ホーム', href: root_path
         expect(page).to have_link '使い方', href: about_path
         expect(page).to have_link 'ユーザー一覧', href: users_path
-        expect(page).to have_link 'プロフィール', href: user_path(user)
-        expect(page).to have_link 'ログアウト', href: logout_path
+        expect(page).to have_link 'チャットを探す', href: chat_rooms_path
+        expect(page).to have_link '参加チャット', href: chatlists_user_path(user)
         expect(page).to have_link '個人チャット履歴', href: dmlists_user_path(user)
+        expect(page).to have_link 'プロフィール', href: user_path(user)
+        expect(page).to have_link '編集', href: edit_user_path(user)
+        expect(page).to have_link 'ログアウト', href: logout_path
         expect(page).not_to have_link 'ログイン', href: login_path
+        expect(page).not_to have_link '新規登録', href: signup_path
       end
     end
   end
